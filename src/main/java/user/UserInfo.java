ba02c9d2816fefca7ab3d19693feb8a27a10dbc1
Denis T, Kevin Carroll (K.) <kdenist+ford@ford.com>
2025-10-16 16:45:02 +0530

    by kevin

--- src/main/java/user/UserInfo.java
+++ src/main/java/user/UserInfo.java
@@ -29,7 +29,7 @@ public class UserInfo {
     @ManyToOne
     private Region region;
 
-    //doubt need to be asked :Dharshan
+    //doubt need to be asked :Dharshan,Kevin
     @OneToOne
     @JoinColumn(name = "managerId",referencedColumnName = "userId")
     private UserInfo manager;
